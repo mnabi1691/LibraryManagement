d10140ac0b4e7bbd3c4c277aa13ec5c9ff0456a4
Mahfuz Nabi <nabi.mahfuz@gmail.com>
2020-05-11 13:06:39 +0600

    trying tracker

--- LibraryManagement/Helper/CryptoHelper.cs
+++ LibraryManagement/Helper/CryptoHelper.cs
@@ -11,7 +11,8 @@ namespace LibraryManagement.Helper
         public static string Hash(string value)
         {
             return System.Convert.ToBase64String(
-                System.Security.Cryptography.SHA256.Create().ComputeHash(
+                System.Security.Cryptography.
+                SHA256.Create().ComputeHash(
                     Encoding.UTF8.GetBytes(value)));
         }
     }
